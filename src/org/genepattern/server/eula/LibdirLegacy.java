f51ca8930e4aa7c325226af88b75d89c899b69ad
Peter Carr <pcarr@broadinstitute.org>
2015-10-22 15:08:46 -0400

    LibdirLegacy update for improved testing; set Hibernate session and GpConfig in constructor GP-5902

--- src/org/genepattern/server/eula/LibdirLegacy.java
+++ src/org/genepattern/server/eula/LibdirLegacy.java
@@ -6,6 +6,10 @@ package org.genepattern.server.eula;
 import java.io.File;
 
 import org.apache.log4j.Logger;
+import org.genepattern.server.config.GpConfig;
+import org.genepattern.server.config.GpContext;
+import org.genepattern.server.config.ServerConfigurationFactory;
+import org.genepattern.server.database.HibernateSessionManager;
 import org.genepattern.server.eula.InitException;
 import org.genepattern.server.webservice.server.DirectoryManager;
 
@@ -18,15 +22,36 @@ import org.genepattern.server.webservice.server.DirectoryManager;
  * @author pcarr
  */
 public class LibdirLegacy implements LibdirStrategy {
-    final static private Logger log = Logger.getLogger(LibdirLegacy.class);
+    private static final Logger log = Logger.getLogger(LibdirLegacy.class);
 
-    //@Override
+    final HibernateSessionManager dbMgr;
+    final GpConfig gpConfig;
+    final GpContext serverContext;
+
+    /** @deprecated should pass in Hibernate session, GpConfig and GpContext **/
+    public LibdirLegacy() {
+        this( org.genepattern.server.database.HibernateUtil.instance(), 
+            ServerConfigurationFactory.instance(),
+            GpContext.getServerContext() );
+    }
+
+    public LibdirLegacy(final HibernateSessionManager dbMgr, final GpConfig gpConfig) {
+        this(dbMgr, gpConfig, GpContext.getServerContext());
+    }
+
+    public LibdirLegacy(final HibernateSessionManager dbMgr, final GpConfig gpConfig, final GpContext serverContext) {
+        this.dbMgr=dbMgr;
+        this.gpConfig=gpConfig;
+        this.serverContext=serverContext;
+    }
+
+    @Override
     public File getLibdir(final String moduleLsid) throws InitException {
         File tasklibDir = null;
         //TODO: implement safer method, e.g. File tasklibDir = DirectoryManager.getTaskLibDirFromCache(moduleLsid);
         //    getLibDir automatically creates a directory on the file system; it's possible to cause problems if there is bogus input
         try {
-            String path = DirectoryManager.getLibDir(moduleLsid);
+            String path = DirectoryManager.getLibDir(dbMgr, gpConfig, serverContext, moduleLsid);
             if (path != null) {
                 tasklibDir = new File(path);
             }
