ba601a884aec09af5089be8dc0c98b440977849b
Peter Carr <pcarr@broadinstitute.org>
2015-11-06 21:17:18 -0500

    JobPermissionsFactory#createJobPermissionsFromDb, throws DbException GP-5902

--- src/org/genepattern/server/JobPermissionsFactory.java
+++ src/org/genepattern/server/JobPermissionsFactory.java
@@ -3,6 +3,7 @@
  *******************************************************************************/
 package org.genepattern.server;
 
+import org.apache.log4j.Logger;
 import org.genepattern.server.auth.IGroupMembershipPlugin;
 import org.genepattern.server.database.HibernateSessionManager;
 import org.genepattern.server.database.HibernateUtil;
@@ -11,20 +12,30 @@ import org.genepattern.server.database.HibernateUtil;
  * Factory method(s) for initializing JobPermissions flags for a given user for a given job.
  */
 public class JobPermissionsFactory {
+    private static final Logger log = Logger.getLogger(JobPermissionsFactory.class);
 
     /** @deprecated should pass in a DB session and GroupMembership */
-    public static final JobPermissions createJobPermissionsFromDb(final boolean isAdmin, final String userId, final int jobNumber) {
+    public static final JobPermissions createJobPermissionsFromDb(final boolean isAdmin, final String userId, final int jobNumber) 
+    throws DbException
+    {
         return createJobPermissionsFromDb(
                 HibernateUtil.instance(),
                 UserAccountManager.instance().getGroupMembership(),
                 isAdmin, userId, jobNumber);
     }
 
-    public static final JobPermissions createJobPermissionsFromDb(final HibernateSessionManager mgr, final IGroupMembershipPlugin groupInfo, final boolean isAdmin, final String userId, final int jobNumber) {
+    public static final JobPermissions createJobPermissionsFromDb(final HibernateSessionManager mgr, final IGroupMembershipPlugin groupInfo, final boolean isAdmin, final String userId, final int jobNumber) 
+    throws DbException
+    {
         final boolean isInTransaction=mgr.isInTransaction();
         try {
             PermissionsHelper perm = new PermissionsHelper(mgr, groupInfo, isAdmin, userId, jobNumber);
             return toJobPermissions(perm);
+        }
+        catch (Throwable t) {
+            final String message="Error getting permission for job="+jobNumber+", userId="+userId;
+            log.error(message, t);
+            throw new DbException(message+": "+t.getLocalizedMessage()); 
         }
         finally {
             if (!isInTransaction) {
